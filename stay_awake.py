07d5d3438c805ffb1fe568248356f723dcfa07af
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2019-06-27 13:57:36 +0100

    simplified script a bit

---
+++ stay_awake.py
@@ -0,0 +1,26 @@
+import pyautogui # used to move cursor
+import time # used for sleep()
+from datetime import datetime # used to display current time
+import itertools as it # provides tools for iterators, like count()
+
+# disable pyautogui's default failsafe, which breaks script when
+# mouse is moved to the top-left corner of the screen
+pyautogui.FAILSAFE = True
+
+# set how often movement should occur, in minutes
+interval = 3
+
+# infinite loop
+for i in it.count():
+
+    # wait interval many minutes
+    time.sleep(interval * 60)
+    
+    # move cursor one pixel down when i is even, and one pixel
+    # up when i is odd
+    if i % 2 == 0:
+        pyautogui.move(0, 1)
+    else:
+        pyautogui.move(0, -1)
+    
+    print(f"Movement made at {datetime.now().time()}"
