0ae6d3f4d4a30796e5b0290f3967e6187ae85976
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2019-07-11 14:54:25 +0100

    added shifts

--- stay_awake.py
+++ stay_awake.py
@@ -2,6 +2,7 @@ import pyautogui # used to move cursor
 import time # used for sleep()
 from datetime import datetime # used to display current time
 import itertools as it # provides tools for iterators, like count()
+import numpy as np # used here for arange() over range()
 
 # disable pyautogui's default failsafe
 pyautogui.FAILSAFE = False
@@ -12,14 +13,19 @@ interval = 3
 # infinite loop
 for i in it.count():
 
-    # wait interval many minutes
+    # wait 'interval' many minutes
     time.sleep(interval * 60)
+
+    # press shift a couple of times
+    pyautogui.press('shift')
+    pyautogui.press('shift')
+    pyautogui.press('shift')
     
     # move cursor one pixel down when i is even, and one pixel
     # up when i is odd
     if i % 2 == 0:
-        pyautogui.move(0, 1)
+            pyautogui.move(0, 1)
     else:
-        pyautogui.move(0, -1)
+            pyautogui.move(0, -1)
     
     print(f"Movement made at {datetime.now().time()}")
